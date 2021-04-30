bee9ed5d35e8a34b20776a8e260f4f3428cd7784
Maxim Komar <maxim-komar@mail.ru>
2021-04-30 08:44:49 +0300

    Rust by Example: Custom types, Enum, Testcase: Linked list

---
+++ rust-by-example/custom-types/enum/testcase_linked_list/testcase_linked_list.rs
@@ -0,0 +1,51 @@
+use crate::List::*;
+
+enum List {
+    // Cons: tuple struct that wraps an element and a pointer to the next node
+    Cons(u32, Box<List>),
+    // Nil: a node that signified the end of the linked list
+    Nil,
+}
+
+impl List {
+    fn new() -> List {
+        Nil
+    }
+
+    fn prepend(self, elem: u32) -> List {
+        Cons(elem, Box::new(self))
+    }
+
+    fn len(&self) -> u32 {
+        // `self` has to be matched, because the behaviour of this method
+        // depends on the variant of `self`
+        // `self` has type `&List`, and `*self` has type `List`, matching on a
+        // concrete type `T` is preferred over a match on a reference `&T`
+        match *self {
+            Cons(_, ref tail) => 1 + tail.len(),
+            Nil => 0
+        }
+    }
+
+    fn stringify(&self) -> String {
+        match *self {
+            Cons(head, ref tail) => {
+                format!("{}, {}", head, tail.stringify())
+            },
+            Nil => {
+                format!("Nil")
+            },
+        }
+    }
+}
+
+fn main() {
+    let mut list = List::new();
+
+    list = list.prepend(1);
+    list = list.prepend(2);
+    list = list.prepend(3);
+
+    println!("linked list has length: {}", list.len());
+    println!("{}", list.stringify());
+}
