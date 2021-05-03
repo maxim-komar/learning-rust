684757134b94fde12b16b61a1cb4c715712ae5fb
Maxim Komar <maxim-komar@mail.ru>
2021-05-03 20:11:42 +0300

    Rust by Example: Scope, Ownership and moves

---
+++ rust-by-example/scope/move/move.rs
@@ -0,0 +1,55 @@
+// bacause variables are in charge of freeing their own resources,
+// resources can only have one owner. This also prevents resources
+// from being freed more than once. Note that not all variables own
+// resources (e.g. references)
+
+// when doing assignments (`let x = y`) or passing function arguments
+// by value (`foo(x)`), the ownership of the resources is transferred.
+// In Rust-speak, this is known as a move.
+//
+// After moving resources, the previous owner can no longer be used. This
+// avoids creating dangling pointers
+
+fn destroy_box(c: Box<i32>) {
+    println!("Destroying a box that contains {}", c);
+
+    // `c` is destroyed and the memory freed
+}
+
+fn main() {
+    // stack allocated integer
+    let x = 5u32;
+
+    // *Copy* `x` into `y`
+    let y = x;
+
+    // both values can be independently used
+    println!("x is {}, and y is {}", x, y);
+
+    // `a` is a pointer to a heap allocated integer
+    let a = Box::new(5i32);
+
+    println!("a contains: {}", a);
+
+    // *Move* `a` into `b`
+    let b = a;
+
+    // the pointer address of `a` is copied (not the data) into `b`
+    // Both are now pointers to the same heap allocated data, but
+    // `b` now owns it
+
+    // Error! `a` can no longer access the data, because it no longer owns
+    // the heap memory
+    // println!("a contains: {}", a);
+
+
+    // this function takes ownership of the heap allocated memory from `b`
+    destroy_box(b);
+
+    // since the heap memory has been freed at this point, this action would
+    // result in dereferencing freed memory, but it's forbidden by the 
+    // compiler
+    //
+    // Error! same reason as the previous Error
+    // println!("b contains: {}", b);
+}
