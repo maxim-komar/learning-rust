8d793d2347112a4b56f08410d0fa3cd40faa9d4c
Maxim Komar <maxim-komar@mail.ru>
2021-05-04 16:02:42 +0300

    Rust by Example: Error handling, Option & unwrap, Combinators: map

---
+++ rust-by-example/error/option_unwrap/map/map.rs
@@ -0,0 +1,64 @@
+// `match` is a valid method for handling `Option`. However, you may
+// eventually find heavy usage tedious, especially with operations only 
+// valid with an input. In these cases, combinators can be used to 
+// manage control flow in a modular fashion.
+// 
+// `Option` has a buil in method called `map()`, a vombinator for the simple
+// mapping of `Some -> Some` and `None -> None`. Multiple `map()` calls can
+// be chained together for even more flexibility.
+//
+// In the following example, `process()` replaces all functions previous
+// to it while staying compact
+
+#![allow(dead_code)]
+
+#[derive(Debug)] enum Food { Apple, Carrot, Potato }
+
+#[derive(Debug)] struct Peeled(Food);
+#[derive(Debug)] struct Chopped(Food);
+#[derive(Debug)] struct Cooked(Food);
+
+fn peel(food: Option<Food>) -> Option<Peeled> {
+    match food {
+        Some(food) => Some(Peeled(food)),
+        None       => None,
+    }
+}
+
+fn chop(peeled: Option<Peeled>) -> Option<Chopped> {
+    match peeled {
+        Some(Peeled(food)) => Some(Chopped(food)),
+        None       => None,
+    }
+}
+
+fn cook(chopped: Option<Chopped>) -> Option<Cooked> {
+    chopped.map(|Chopped(food)| Cooked(food))
+}
+
+fn process(food: Option<Food>) -> Option<Cooked> {
+    food.map(|f|          Peeled(f))
+        .map(|Peeled(f)|  Chopped(f))
+        .map(|Chopped(f)| Cooked(f))
+}
+
+fn eat(food: Option<Cooked>) {
+    match food {
+        Some(food) => println!("Mmm. I love {:?}", food),
+        None       => println!("Oh no! It wasn't edible"),
+    }
+}
+
+fn main() {
+    let apple = Some(Food::Apple);
+    let carrot = Some(Food::Carrot);
+    let potato = None;
+
+    let cooked_apple = cook(chop(peel(apple)));
+    let cooked_carrot = cook(chop(peel(carrot)));
+    let cooked_potato = process(potato);
+
+    eat(cooked_apple);
+    eat(cooked_carrot);
+    eat(cooked_potato);
+}
